fc691ef59b5475f6dadfcccd65e45ede123332d5
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2025-09-04 17:57:57 -0400

    init repo

---
+++ nix/build.nix
@@ -0,0 +1,24 @@
+{ stdenv, quarto, texliveFull, texworks, texstudio, librsvg, chromium, python3, pythonPkg, ... }:
+stdenv.mkDerivation {
+  pname = "CWRUnix-presentations";
+  version = "0.0.1";
+  src = ./../src;
+
+  nativeBuildInputs = [
+    (python3.withPackages (pythonPkg))
+    quarto
+    texliveFull
+    texworks
+    texstudio
+    librsvg
+    chromium
+  ];
+
+  buildPhase = ''
+    quarto render .
+  '';
+  installPhase = ''
+    mkdir -p $out
+    cp -r _output/* $out
+  '';
+}
